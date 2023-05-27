972c563bd22065dffa76413c68428460a5aeee2c
David Bomba <turbo124@gmail.com>
2023-05-27 17:22:08 +1000

    Minor clean up

--- src/Rule.php
+++ src/Rule.php
@@ -2,21 +2,42 @@
 
 namespace Turbo124\BotLicker;
 
+use Illuminate\Support\Carbon;
+use Turbo124\BotLicker\Models\BotlickerRule;
+
 class Rule
 {
     private $protected_ips = [
         '127.0.0.1',
     ];
 
-    public string $ip = '';
+    private string $rule = '';
 
-    public function matches(string $ip)
+    private string $action = 'ban';
+
+    private ?Carbon $expiry;
+
+    public function matches(string $rule, ?Carbon $expiry)
     {
 
+        if(strlen($rule < 3))
+            return;
+
+        $this->rule = $rule;
+        $this->expiry = $expiry;
+
+        return $this;
     }
 
-    public function ban()
+    public function ban(): void
     {
-
+        
+        BotlickerRule::on(config('bot-licker.db_connection'))
+                     ->insert([
+                        'matches'   => $this->rule,
+                        'action'    => $this->action,
+                        'expiry'    => $this->expiry,
+                     ]);
+                     
     }
 }

c61d244ebfccf81c60a02ad2247be3b65ab47a4c
David Bomba <turbo124@gmail.com>
2023-05-27 17:09:26 +1000

    Minor clean up

--- src/Http/Middleware/QueryLog.php
+++ src/Http/Middleware/QueryLog.php
@@ -15,12 +15,16 @@ class QueryLog
 
     public function terminate($request, $response)
     {
-        // Store the session data...
-        BotlickerLog::on(config('bot-licker.db_connection'))->insert([
-            'ip' => $request->ip(),
-            'url' => substr(urldecode($request->getRequestUri()), 0, 180),
-        ]);
+        $uri = urldecode($request->getRequestUri());
 
+            if(strlen($uri) > 3) {
+
+                // Store the session data...
+                BotlickerLog::on(config('bot-licker.db_connection'))->insert([
+                    'ip' => $request->ip(),
+                    'url' => substr($uri, 0, 180),
+                ]);
+            }
     }
 
 }
